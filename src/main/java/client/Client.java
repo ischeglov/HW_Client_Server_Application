a8c5ff48113c079b5686271d469899f7877ea34f
ischeglov <Ivan_mix@mail.ru>
2023-10-22 15:49:16 +0300

    add basic logic

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,2 +1,22 @@
-package client;public class Client {
+package client;
+
+import server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket("localhost", Server.LOCALHOST_PORT);
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            writer.println("Ivan!");
+            System.out.println(reader.readLine());
+        }
+    }
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,2 +1,32 @@
-package server;public class Server {
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static final Integer LOCALHOST_PORT = 8088;
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(LOCALHOST_PORT)) {
+            System.out.println("Сервер стартанул");
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter outWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader inReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ) {
+                    String name = inReader.readLine();
+                    System.out.println(String.format("Подключение установлено. Информация от клиента: " +
+                            "Hi %s, your port is %d", name, clientSocket.getPort()));
+                    outWriter.println(clientSocket.getPort());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
